750033910a1f8b2a82dcff9f017f83458dc83c49
Tess Griffin <tessjgriffin@gmail.com>
2017-06-10 17:32:31 -0400

    Add Admin create and edit for users
    Added the pages and controller actions for an admin to be able to create and edit users.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,6 +9,34 @@ module Admin
       respond_with(@users)
     end
 
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "User was created"
+        redirect_to admin_users_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:notice] = "User was updated."
+        redirect_to admin_users_path
+      else
+        render :edit
+      end
+    end
+
     def impersonate
       user = User.find(params[:id])
       track_impersonation(user, 'Start')
@@ -33,5 +61,9 @@ module Admin
         impersonated_by_email: true_user.email,
       )
     end
+
+    def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
   end
 end
